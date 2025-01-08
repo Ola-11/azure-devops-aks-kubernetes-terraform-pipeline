16788740e5e2ecee6f89f93f56f0e7fae29c6338
Oladeinde Oluwaseun <90369121+Ola-11@users.noreply.github.com>
2025-01-08 16:47:00 -0500

    Update 07-aks-cluster.tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -3,14 +3,13 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   location            = azurerm_resource_group.aks_rg.location
   name                = "${azurerm_resource_group.aks_rg.name}-cluster"
   resource_group_name = azurerm_resource_group.aks_rg.name
-  kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
 
 
   default_node_pool {
     name       = "systempool"
     vm_size    = "Standard_DS2_v2"
-    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
+    
     availability_zones   = [1, 2, 3]
     enable_auto_scaling  = true
     max_count            = 3
@@ -44,8 +43,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 
 # RBAC and Azure AD Integration Block
-role_based_access_control {
-  enabled = true
+role_based_access_control_enabled = true
   azure_active_directory {
     managed                = true
     admin_group_object_ids = [azuread_group.aks_administrators.id]
